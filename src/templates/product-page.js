bfde2acbfdb6f2372a4798b7790ed51c4dbb7624
Jason Woods <jasonw@crs-jwoo-mac.lan>
2020-05-19 21:14:58 -0400

    fixed gallery h2 heading

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -31,8 +31,9 @@ export const ProductPageTemplate = ({
       <h2
         className="has-text-weight-bold is-size-1"
         style={{
-          boxShadow: '0.5rem 0 0 #48c774, -0.5rem 0 0 #48c774',
-          backgroundColor: '#48c774',
+          boxShadow: '0.5rem 0 0 rgb(72, 199, 116, 0.5), -0.5rem 0 0 rgb(72, 199, 116, 0.5)',
+          backgroundColor: 'rgb(72, 199, 116, 0.5)',
+          textShadow: `2px 2px #333333`,
           color: 'white',
           padding: '1rem',
         }}
