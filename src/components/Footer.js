f7195e2b6c35d10ee7004736b5826c450709a514
Jason Woods <jason.a.woods@gmail.com>
2023-07-10 11:37:04 +0000

    initial changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,9 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        {/* <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div> */}
+        <div className="content has-text-centered">
+          2010 Chapin Rd, Chapin SC 29036
+        </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
